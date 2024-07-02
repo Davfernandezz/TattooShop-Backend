7ee0053825b2dc34ebe2fc2d7dda025ec4ec41b8
David Fernadez <daffevafernandez@gmail.com>
2024-07-02 17:52:18 +0200

    feat: services migration

--- src/database/migrations/1719851655446-services.ts
+++ src/database/migrations/1719851655446-services.ts
@@ -1,11 +1,37 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Services1719851655446 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "services",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "service_name",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                        length: "255",
+                    }
+                ],
+            }),
+            true
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('services')
     }
 
 }
